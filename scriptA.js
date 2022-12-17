f79634943a9b0f1b9377f9e621919f86f10ff8ae
Sonu Pant007 <60938647+sonuoo7@users.noreply.github.com>
2022-12-17 21:00:27 +0530

    Add files via upload

---
+++ scriptA.js
@@ -0,0 +1,12 @@
+function geoFindme() {
+  function success(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+  
+    localStorage.setItem('lat',latitude)
+    localStorage.setItem('long',longitude)
+    window.location.href="api.html" // link the 2page
+
+    }
+    navigator.geolocation.getCurrentPosition(success);
+}
